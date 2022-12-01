dc2414ff6d97047c25156a4471e6f6cea9875562
Axel Demborg <axel.demborg@gmail.com>
2022-12-01 20:13:11 +0100

    day 1 part 2

--- src/1.tsx
+++ src/1.tsx
@@ -1,16 +1,20 @@
 export const Day1 = (props: { input: string }) => {
+  const caloriesPerElf = props.input.split("\n\n").map((elf) =>
+    elf
+      .split("\n")
+      .map(parseFloat)
+      .reduce((a, b) => a + b)
+  );
   return (
     <div>
       <h2>Day 1</h2>
       <h3>Part 1</h3>
-      {Math.max(
-        ...props.input.split("\n\n").map((elf) =>
-          elf
-            .split("\n")
-            .map(parseFloat)
-            .reduce((a, b) => a + b)
-        )
-      )}
+      {Math.max(...caloriesPerElf)}
+      <h3>Part 2</h3>
+      {caloriesPerElf
+        .sort((a, b) => a - b)
+        .slice(-3)
+        .reduce((a, b) => a + b)}
     </div>
   );
 };
