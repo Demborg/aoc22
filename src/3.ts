2c44f14b86a13b1dade9a57621db0a9f75c84618
Axel Demborg <axel.demborg@gmail.com>
2022-12-03 07:01:21 +0100

    refactor for part2

--- src/3.ts
+++ src/3.ts
@@ -1,11 +1,18 @@
 import { Result } from "./types";
+
+const priority = (item?: string): number => {
+  const n = item!.charCodeAt(0);
+  return n > 96 ? n - 96 : n - 65 + 27;
+};
+
 export const day3 = (input: string): Result => {
-  const rugsacks = input
-    .split("\n")
+  const rugsacks = input.split("\n");
+
+  const p1 = rugsacks
     .map((r) => [r.slice(0, r.length / 2), r.slice(r.length / 2)])
     .map((r) => [...r[0]].find((c) => r[1].includes(c)))
-    .map((c) => c!.charCodeAt(0))
-    .map((n) => (n > 96 ? n - 96 : n - 65 + 27));
-  console.log(rugsacks);
-  return [rugsacks.reduce((a, b) => a + b), -1];
+    .map(priority)
+    .reduce((a, b) => a + b);
+
+  return [p1, -1];
 };
