dbbc9eed7f44ee863dfe674f6b30e6e1e43250d6
Axel Demborg <axel.demborg@gmail.com>
2022-12-04 09:12:30 +0100

    refactor for part 2

--- src/4.ts
+++ src/4.ts
@@ -1,12 +1,18 @@
 import { Result } from "./types";
 export const day4 = (input: string): Result => {
-  const count = input
+  const parsed = input
     .split("\n")
-    .map((pair) => pair.split(",").map((elf) => elf.split("-").map(parseFloat)))
-    .map(
-      (pair) =>
-        (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1]) ||
-        (pair[1][0] <= pair[0][0] && pair[1][1] >= pair[0][1])
-    ).reduce((a, b) => a + (b ? 1 : 0), 0);
-  return [count, -1];
+    .map((pair) =>
+      pair.split(",").map((elf) => elf.split("-").map(parseFloat))
+    );
+  return [
+    parsed
+      .map(
+        (pair) =>
+          (pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1]) ||
+          (pair[1][0] <= pair[0][0] && pair[1][1] >= pair[0][1])
+      )
+      .reduce((a, b) => a + (b ? 1 : 0), 0),
+    -1,
+  ];
 };
