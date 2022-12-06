a5d3fd1bdf792623faac666285e56ef44e203fef
Axel Demborg <axel.demborg@gmail.com>
2022-12-06 08:06:26 +0100

    prettier

--- src/6.ts
+++ src/6.ts
@@ -1,17 +1,17 @@
 import { Result } from "./types";
 
 const startOfSymbol = (data: string, length: number) => {
-    const buffer =  [...data.slice(0, length - 1)]
-    for (let i = length - 1; i < data.length; i++) {
-        buffer.push(data[i]);
-        if (new Set(buffer).size == length) {
-            return i + 1
-        }
-        buffer.shift()
+  const buffer = [...data.slice(0, length - 1)];
+  for (let i = length - 1; i < data.length; i++) {
+    buffer.push(data[i]);
+    if (new Set(buffer).size == length) {
+      return i + 1;
     }
-    return undefined
-}
+    buffer.shift();
+  }
+  return undefined;
+};
 
 export const day6 = (input: string): Result => {
-    return [startOfSymbol(input, 4) ?? -1, startOfSymbol(input, 14) ?? -1]
-}
+  return [startOfSymbol(input, 4) ?? -1, startOfSymbol(input, 14) ?? -1];
+};
